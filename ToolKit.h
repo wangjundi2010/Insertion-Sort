7d282bcc5f1999cb0932a6463b4f7539d9b73ccc
wangjundi2010 <wangjundi2010@users.noreply.github.com>
2015-01-28 13:38:09 -0600

    Toolkit.h
    Header file of Toolkit

---
+++ ToolKit.h
@@ -0,0 +1,12 @@
+#ifndef TOOLKITS_H
+#define TOOLKITS_H
+
+#include <iostream>
+#include "Insertion_Sort.h"
+using namespace std;
+using namespace INSERTION_SORT;
+
+int GetSize();
+void InputValue(int SizeOfList, insertion_sort& objMySort);
+
+#endif
